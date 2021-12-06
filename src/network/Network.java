3abe181ac31892538cb31d397dd5e062932df07d
Adham Magdy <60877873+AdhamMagdyA@users.noreply.github.com>
2021-12-06 10:57:53 +0200

    created the semaphore class

--- src/network/Network.java
+++ src/network/Network.java
@@ -1,12 +1,30 @@
 package network;
 
+class Semaphore{
+ private static final int maxDevices = 3;  // whatever maximum number
+ private static int availableDevices = maxDevices;
+
+ public synchronized void Wait(){  
+  while(availableDevices==0){  
+   try{ wait(); }
+   catch(InterruptedException e){}
+  }
+  availableDevices--;  
+ }
+
+ public synchronized void Signal(){
+  while(availableDevices==maxDevices){
+   try{ wait(); }
+   catch(InterruptedException e){}
+  }
+  availableDevices++;  
+ }
+}
+
 public class Network {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
